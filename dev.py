b8bac428443aca0497a3b10d58e61c9b9cca615a
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2020-03-21 12:52:16 +0100

    limits

--- dev.py
+++ dev.py
@@ -3,4 +3,6 @@ from dataHandler import DataHandler
 dataHandler = DataHandler()
 dataHandler.loadData()
 
-print(dataHandler.data['confirmed'])
+#print(dataHandler.data['confirmed'].columns())
+df = dataHandler.data['confirmed']
+germany=df[df['Country/Region']=="Germany"]
