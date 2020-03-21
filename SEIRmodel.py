fb79d9184fe641d39b79a219d04c36be424d8aac
FelixKratz <felix.kratz@tu-dortmund.de>
2020-03-21 19:18:36 +0100

    make fitting more robust

--- SEIRmodel.py
+++ SEIRmodel.py
@@ -62,7 +62,7 @@ class SEIRModel:
 
 if __name__ == "__main__" :
     model = SEIRModel({
-                    'beta': 0.9,  # The parameter controlling how often a susceptible-infected contact results in a new exposure.
+                    'beta': 0.6,  # The parameter controlling how often a susceptible-infected contact results in a new exposure.
                     'gamma':0.2,  # The rate an infected recovers and moves into the resistant phase.
                     'sigma': 0.5, # The rate at which an exposed person becomes infective.
                     'mu': 0,      # The natural mortality rate (this is unrelated to disease). This models a population of a constant size,
@@ -70,12 +70,12 @@ if __name__ == "__main__" :
                     'dt': 0.1,
                     'S0': 83e6,
                     'E0': 0,
-                    'I0': 1,
+                    'I0': 20,
                     'Re0': 0,
                     'darkrate': 0.05, # erstmal China studie # Quelle: Linton MN, Kobayashi T, Yang Y, Hayashi K, Akhmetzhanov RA, Jung S-m, et al. Incubation Period and Other Epidemiological Characteristics of 2019 Novel Coronavirus Infections with Right Truncation: A Statistical Analysis of Publicly Available Case Data. Journal of clinical medicine. 2020.
                     'hardrate': 0.154, # WHO studie:  Novel Coronavirus (2019-nCoV). (PDF; 0,9 MB) Situation Report – 18. WHO, 7. Februar 2020, abgerufen am 8. Februar 2020.
                     'deathrate': 0.034 # WHO :  Eröffnungsrede des WHO-Generaldirektors – Pressekonferenz zu COVID-19 – 3. März 2020. WHO, 3. März 2020, abgerufen am 6. März 2020 (englisch).
                     })
 
-    prediction = model.compute(days=30)
-    print(model.get_daily_numbers())
+    prediction = model.compute(days=16)
+    print(model.get_daily_numbers()['D'])
