8c1bf1715069ae00ac0225ed0cd849c35b2060ca
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2020-03-21 16:22:58 +0100

    Visualizer class, tested for germany

--- dev.py
+++ dev.py
@@ -2,7 +2,5 @@ from dataHandler import DataHandler
 
 dataHandler = DataHandler()
 dataHandler.loadData()
-
-#print(dataHandler.data['confirmed'].columns())
-df = dataHandler.data['confirmed']
-germany=df[df['Country/Region']=="Germany"]
+tmp=dataHandler.filterForCountry("Germany")["confirmed"]
+print(tmp)
--- visualize.py
+++ visualize.py
@@ -1,38 +1,35 @@
 import numpy as np
-
-from bokeh.plotting import figure, show, output_file
+import pandas as pd
+from dataHandler import DataHandler
+from bokeh.plotting import figure, show, output_file, save
 from bokeh.models import ColumnDataSource, HoverTool
+import os
+class Visualizer:
+    def __init__(self, dataHandler):
+        dataHandler.loadData()
+    def visualize(self, country):
+        for item in ["confirmed", "deaths", "recovered"]:
+            hover = HoverTool(tooltips=[
+                ("Sepal length", "@sepal_length"),
+                ("Sepal width", "@sepal_width"),
+                ("Petal length", "@petal_length"),
+                ("Species", "@species")
+            ])
+            p = figure(title="", plot_height=500, plot_width=500,
+                   tools=[hover, "pan,reset,wheel_zoom"])
+
+            p.xaxis.axis_label = 't/days'
+            p.yaxis.axis_label = '# {} cases'.format(item)
+            y_data = dataHandler.filterForCountry(country)[item][0]
+            t=np.linspace(1, len(y_data), len(y_data))
+            p.line(t, y_data, legend_label="blup", line_width=2)
+            if not os.path.exists("docs/plots/{}/".format(country)):
+                 os.makedirs("docs/plots/{}/".format(country))
+            output_file('docs/plots/{}/{}_cases.html'.format(country, item))
+            save(p)
+
+#####***************
 
 
-def lin(x):
-    return 3*x
-
-
-x = np.linspace(0, 10)
-
-
-
-
-
-hover = HoverTool(tooltips=[
-    ("Sepal length", "@sepal_length"),
-    ("Sepal width", "@sepal_width"),
-    ("Petal length", "@petal_length"),
-    ("Species", "@species")
-])
-
-p = figure(title="lin", plot_height=500, plot_width=500,
-           tools=[hover, "pan,reset,wheel_zoom"])
-
-p.xaxis.axis_label = 'x'
-p.yaxis.axis_label = 'y'
-
-p.line(x, lin(x), legend_label="blup", line_width=2)
-
-output_file('docs/plots/testplot.html')
-
-
-#class Visualizer:
-#    def __init__(self, data):
-#        self.data=data
-#    def visualize(self, data)
+dataHandler = DataHandler()
+Visualizer(dataHandler).visualize("Germany")
