857210d08acd86c86759bef9338600ae3258a61e
Lars Möllerherm <lars.moellerherm@tu-dortmund.de>
2020-03-22 11:20:13 +0100

    change of D

--- SEIRmodel.py
+++ SEIRmodel.py
@@ -38,7 +38,7 @@ class SEIRModel:
                     self.set_actual(self.params)
                 
                 self.actual.N = self.actual.S + self.actual.E + self.actual.I + self.actual.R
-                self.actual.D = self.actual.I*self.params['darkrate']
+                self.actual.D = self.actual.I*self.params['darkrate'] + self.actual.R*self.params['darkrate']
                 self.actual.hard_course = self.actual.I*self.params['darkrate']*self.params['hardrate']
                 self.actual.deadly_course = self.actual.I*self.params['darkrate']*self.params['deathrate']
             self.series = self.series.append(self.actual,ignore_index=True)
