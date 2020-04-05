5df8798b060daa9d693546b9e89d707196c30465
FelixKratz <felix.kratz@tu-dortmund.de>
2020-04-05 13:26:39 +0200

    new data

--- visualize.py
+++ visualize.py
@@ -352,4 +352,4 @@ dataHandler = DataHandler()
 
 model = SEIRModel({'beta': 0.4458832735928616, 'gamma': 0.04142817668313051, 'sigma': 0.13654249480116884, 'mu': 0, 'nu': 0, 'dt': 0.1, 'S0': 83000000.0, 'E0': 0, 'I0': 46.53585481679889, 'Re0': 0, 'darkrate': 0.05, 'hardrate': 0.154, 'deathrate': 0.034}
 )
-Visualizer(dataHandler, model, steps=150, death_rate=0.02).visualize("Germany")
+Visualizer(dataHandler, model, steps=100, death_rate=0.02).visualize("Germany")
